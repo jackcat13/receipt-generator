68499e9168986a43ebedbdf705634c1f47a9756e
jackcat13 <christophehenry11@gmail.com>
2023-02-21 18:02:39 +0000

    Deploying to gh-pages from @ jackcat13/receipt-generator@63bb41e861bc36fd17d99aa012ee9fb2bbaa4ef3 🚀

--- receipt-generator-a7fa7acceacd18a2.js
+++ receipt-generator-a7fa7acceacd18a2.js
@@ -838,7 +838,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('receipt-generator-2ae552c69a455005_bg.wasm', import.meta.url);
+        input = new URL('receipt-generator-a7fa7acceacd18a2_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
